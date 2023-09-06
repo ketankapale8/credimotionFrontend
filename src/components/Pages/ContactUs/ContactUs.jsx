703189dcf580a6b9d6ab7e072d8a3bd3c27ac756
ketankapale8 <ketan.kapale@gmail.com>
2023-09-06 18:59:56 +0530

    ContactUs

--- src/components/Pages/ContactUs/ContactUs.jsx
+++ src/components/Pages/ContactUs/ContactUs.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import './contactus.scss'
+import './contactus.scss';
+import { Link } from 'react-router-dom';
+import contactusBanner from '../../../assets/contactus/contactusbanner.jpg'
 
 const ContactUs = () => {
   return (
-    <div className='contactus'>ContactUs</div>
+    <div className='contactus'>
+      <div className="bannerContainer">
+        <img src={contactusBanner} alt="contactusbanner"  className='bannerContactus'/>
+      <div className="formContainer">
+        <h1>The Most Trusted </h1>
+        <h1>Vehicle Lifecycle</h1>
+        <h1>  Ownership.</h1>
+        <Link to="/ourservices">
+            <button className="explorebtn">
+                            EXPLORE
+            </button>
+        </Link>
+      </div>
+      </div>
+    </div>
   )
 }
 
