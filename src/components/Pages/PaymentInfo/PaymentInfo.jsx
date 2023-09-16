fab97404b26f8790b8f364d49315a8d339477f2a
ketankapale8 <ketan.kapale@gmail.com>
2023-09-16 13:52:18 +0530

    changes!!

--- src/components/Pages/PaymentInfo/PaymentInfo.jsx
+++ src/components/Pages/PaymentInfo/PaymentInfo.jsx
@@ -10,8 +10,8 @@ const PaymentInfo = () => {
   const user_id  = user._id
   const url ="https://credimotionbackend.vercel.app";
   useEffect(()=>{
-    const data = axios.get(`https://credimotionbackend.vercel.app/api/v1/allorders`)
-    setOrderData(data?.orders.filter(item=> item._id = user_id))
+    const {data} = axios.get(`https://credimotionbackend.vercel.app/api/v1/allorders`)
+    setOrderData(data?.orders?.filter(item=> item?._id = user_id))
   },[])
   console.log(orderData)
 
