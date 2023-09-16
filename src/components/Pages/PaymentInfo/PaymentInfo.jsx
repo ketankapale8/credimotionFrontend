8a4cdac1286bc9795340ccb1dd99788a396ab739
ketankapale8 <ketan.kapale@gmail.com>
2023-09-16 13:47:20 +0530

    changes!!!GetSpecifcOrder

--- src/components/Pages/PaymentInfo/PaymentInfo.jsx
+++ src/components/Pages/PaymentInfo/PaymentInfo.jsx
@@ -5,13 +5,15 @@ import { useContext } from 'react';
 
 const PaymentInfo = () => {
   const {user , setUser , setIsAuthenticated} = useContext(Context);
+  const [orderData , setOrderData] = useState([])
   console.log(user._id)
   const user_id  = user._id
   const url ="https://credimotionbackend.vercel.app";
   useEffect(()=>{
-    const {data} = axios.get(`https://credimotionbackend.vercel.app/api/v1/myorder/${user_id}`)
-    console.log(data)
+    const data = axios.get(`https://credimotionbackend.vercel.app/api/v1/myorder/${user_id}`)
+    setOrderData(data?.orders.filter(item=> item._id = user_id))
   },[])
+  console.log(orderData)
 
 
   
